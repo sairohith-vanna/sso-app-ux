4278ce8ab1fd68a1f104418a39d53c3edc3648f3
Sai <sairohit.vangala@vannainfotech.com>
2020-11-07 23:27:24 +0530

    Add a sample home page

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,30 @@
+import { KeycloakService } from 'keycloak-angular';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  keycloakAuthDetails: any = {
+    authJWTToken: '',
+    authUser: ''
+  };
+
+  constructor(private keycloak: KeycloakService) { }
+
+  ngOnInit(): void {
+    this.keycloak.getToken().then((value) => {
+      this.keycloakAuthDetails.authJWTToken = value;
+    });
+    try {
+      this.keycloakAuthDetails.authUser = this.keycloak.getUsername();
+    } catch (err) {
+      console.log(err);
+      this.keycloakAuthDetails.authUser = 'There was an error fetching the username';
+    }
+  }
+
+}
