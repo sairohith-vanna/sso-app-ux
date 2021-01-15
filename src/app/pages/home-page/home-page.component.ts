28935f039006b4321d396198fe189855cfd182ae
Sai <sairohit.vangala@vannainfotech.com>
2021-01-15 10:13:10 +0530

    Configures with angular-oauth2-oidc library
    The application fetches the well-known from the backend, which has the token endpoint set to the backend application. Eliminates the necessity of having a client-secret in the front-end making it more secure.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { SampleService } from './../../services/sample.service';
-import { KeycloakService } from 'keycloak-angular';
 import { Component, OnInit } from '@angular/core';
 import { FileSaverService } from 'ngx-filesaver';
 import { HttpResponse } from '@angular/common/http';
@@ -19,12 +18,12 @@ export class HomePageComponent implements OnInit {
   private sampleCSVDownloadData: Blob;
   csvDataArray: string[] = [];
 
-  constructor(private keycloak: KeycloakService, private sampleService: SampleService, private fileSaver: FileSaverService) { }
+  constructor(private sampleService: SampleService, private fileSaver: FileSaverService) { }
 
   /**
    * Fetches a csv file from the API.
    */
-  getSampleDownloadFile() {
+  getSampleDownloadFile(): void {
     this.sampleService.getSampleDocument().subscribe({
       next: (response: HttpResponse<any>) => {
         this.sampleCSVDownloadData = response.body;
@@ -38,9 +37,9 @@ export class HomePageComponent implements OnInit {
    * Interprets the csv file as text, and retrieves the second line
    * containing the values. Removes the surrounding double quotes for the values.
    */
-  readCSVFile() {
+  readCSVFile(): void {
     this.sampleCSVDownloadData.text().then((csvTextData) => {
-      var csvValuesLineArray = csvTextData.split("\n")[1].split(",");
+      const csvValuesLineArray = csvTextData.split('\n')[1].split(',');
       this.csvDataArray = csvValuesLineArray.map((value) => value.replace(/^"|"$/g, ''));
     });
   }
@@ -50,20 +49,11 @@ export class HomePageComponent implements OnInit {
    * The usage of FileSaverService requires installation of ngx-filesaver.
    * It can be installed using npm install file-saver ngx-filesaver --save
    */
-  downloadSampleFile() {
-    this.fileSaver.save(this.sampleCSVDownloadData, "credentials.csv");
+  downloadSampleFile(): void {
+    this.fileSaver.save(this.sampleCSVDownloadData, 'credentials.csv');
   }
 
   ngOnInit(): void {
-    this.keycloak.getToken().then((value) => {
-      this.keycloakAuthDetails.authJWTToken = value;
-    });
-    try {
-      this.keycloakAuthDetails.authUser = this.keycloak.getUsername();
-    } catch (err) {
-      console.log(err);
-      this.keycloakAuthDetails.authUser = 'There was an error fetching the username';
-    }
     this.sampleService.getSampleResponse().subscribe({
       next: (response: HttpResponse<any>) => {
         console.log(response);
