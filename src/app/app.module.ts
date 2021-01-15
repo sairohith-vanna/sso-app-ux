b7eaa0f945455476f7ae71dba96727f77b9f8e67
Sai <sairohit.vangala@vannainfotech.com>
2021-01-15 06:56:54 +0530

    Basic changes with auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,21 @@ import { AppComponent } from './app.component';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthModule, LogLevel, OidcConfigService } from 'angular-auth-oidc-client';
+
+// tslint:disable-next-line: typedef
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+      oidcConfigService.withConfig({
+          stsServer: 'https://ksso.aws.srrclouddev.xyz/auth',
+          redirectUrl: 'http://localhost:4200',
+          postLogoutRedirectUri: window.location.origin,
+          clientId: 'demoapp-client',
+          logLevel: LogLevel.Debug,
+          authWellknownEndpoint: 'http://localhost:8081',
+          responseType: 'code'
+      });
+}
 
 @NgModule({
   declarations: [
@@ -16,16 +31,17 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    KeycloakAngularModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthModule.forRoot()
   ],
   providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: KeycloakConfiguration.initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService]
-    }
+    OidcConfigService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: configureAuth,
+            deps: [OidcConfigService],
+            multi: true,
+        }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { KeycloakService } from 'keycloak-angular';
 import { Component, OnInit } from '@angular/core';
 import { FileSaverService } from 'ngx-filesaver';
 import { HttpResponse } from '@angular/common/http';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-home-page',
@@ -19,12 +20,13 @@ export class HomePageComponent implements OnInit {
   private sampleCSVDownloadData: Blob;
   csvDataArray: string[] = [];
 
-  constructor(private keycloak: KeycloakService, private sampleService: SampleService, private fileSaver: FileSaverService) { }
+  constructor(private sampleService: SampleService, private fileSaver: FileSaverService,
+    private oidcSecurityService: OidcSecurityService) { }
 
   /**
    * Fetches a csv file from the API.
    */
-  getSampleDownloadFile() {
+  getSampleDownloadFile(): void {
     this.sampleService.getSampleDocument().subscribe({
       next: (response: HttpResponse<any>) => {
         this.sampleCSVDownloadData = response.body;
@@ -38,7 +40,7 @@ export class HomePageComponent implements OnInit {
    * Interprets the csv file as text, and retrieves the second line
    * containing the values. Removes the surrounding double quotes for the values.
    */
-  readCSVFile() {
+  readCSVFile(): void {
     this.sampleCSVDownloadData.text().then((csvTextData) => {
       var csvValuesLineArray = csvTextData.split("\n")[1].split(",");
       this.csvDataArray = csvValuesLineArray.map((value) => value.replace(/^"|"$/g, ''));
@@ -50,27 +52,21 @@ export class HomePageComponent implements OnInit {
    * The usage of FileSaverService requires installation of ngx-filesaver.
    * It can be installed using npm install file-saver ngx-filesaver --save
    */
-  downloadSampleFile() {
+  downloadSampleFile(): void {
     this.fileSaver.save(this.sampleCSVDownloadData, "credentials.csv");
   }
 
   ngOnInit(): void {
-    this.keycloak.getToken().then((value) => {
-      this.keycloakAuthDetails.authJWTToken = value;
+    this.oidcSecurityService.checkAuth().subscribe((auth) => {
+      console.log('is authenticated', auth);
+      if (!auth) {
+        this.login();
+        this.keycloakAuthDetails.authJWTToken = this.oidcSecurityService.getToken();
+      }
     });
-    try {
-      this.keycloakAuthDetails.authUser = this.keycloak.getUsername();
-    } catch (err) {
-      console.log(err);
-      this.keycloakAuthDetails.authUser = 'There was an error fetching the username';
-    }
-    this.sampleService.getSampleResponse().subscribe({
-      next: (response: HttpResponse<any>) => {
-        console.log(response);
-      },
-      error: (error) => console.log(error)
-    });
+  }
 
-    this.getSampleDownloadFile();
+  login(): void {
+    this.oidcSecurityService.authorize();
   }
 }
